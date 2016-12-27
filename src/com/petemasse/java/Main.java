f177edc5dbb41357136a792dd4ec4ba720b1deaa
peterfmasseiii <pfmasse60@gmail.com>
2016-12-26 21:03:07 -0700

    Initial commit

---
+++ src/com/petemasse/java/Main.java
@@ -0,0 +1,13 @@
+package com.petemasse.java;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Menu m = new Menu();
+        m.menuView();
+    }
+
+
+}
---
+++ src/com/petemasse/java/menu.java
@@ -0,0 +1,14 @@
+package com.petemasse.java;
+
+/**
+ * Created by peterfmasseiii on 12/26/16.
+ */
+class Menu {
+    public void Menu(){
+        System.out.println("Menu Constructor");
+    }
+
+    public static void menuView(){
+        System.out.println("menuView");
+    }
+}
